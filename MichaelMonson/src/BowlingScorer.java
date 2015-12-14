d9882f2683aec4c1fdbc3466da0f2df56b2c08c6
Michael Monson <michaelmonson@ldschurch.org>
2015-12-13 22:35:44 -0700

    Adding Bowling Calculation logic - parsing scoresheet

--- MichaelMonson/src/BowlingScorer.java
+++ MichaelMonson/src/BowlingScorer.java
@@ -9,7 +9,8 @@ public class BowlingScorer {
 	private static final String NORMAL = "N";
 	private static final int PERFECT_GAME = 12;
 	private static final int MAXIMUM_ROLLS = 21;
-	private static Integer[] player1ScoreSheet = {4, 6, 10, 4, 0, 9,1};
+	private static Integer[] player1ScoreSheet = {4, 4, 10, 5, 4, 4, 6, 3, 7, 4, 6, 10, 8, 2, 1, 0, 10, 9, 10, 9, 1};
+	private static Integer[] player2ScoreSheet = {10,10,6,4,8,1,9,0,4,5,10,3,7,2,0};
 	
 	public static void main(String[] args) {
 		
@@ -27,23 +28,28 @@ public class BowlingScorer {
 		ArrayList<Integer> frameTotals = new ArrayList<Integer>();
 		ArrayList<String> frameType = new ArrayList<String>();
 		
-		Integer currentFrame = 1;
+		Integer currentFrame = 1;		
+		int frameRoll = 1;
+		Integer currentFrameTotal = 0;
 		
-		
-		for( int i = 0; i < playerSheet.length - 1; i++) {
-			
-			Integer currentFrameTotal = 0;
-			int frameRoll = 1;
+		for (int i = 0; i < playerSheet.length; i++) {
 			
 			if (currentFrame > 10) {
-				break;
+				break; //End iteration... all ten frames are processed
 			}
-			
+		
+			// Add total points for the frame:
 			if (frameRoll == 1 && playerSheet[i] == STRIKE_VALUE ) {
-//				playerTotal += playerSheet[i];
-				frameTotals.add(STRIKE_VALUE);
+//				playerTotal += playerSheet[i];   // Add up the totals below...
+				currentFrameTotal = playerSheet[i] + playerSheet[i+1] + playerSheet[i+2];
+				frameTotals.add(currentFrameTotal);
 				frameType.add(STRIKE);
-				break;
+				System.out.println("Roll #" + i + " : Strike added.  Points = " + currentFrameTotal);
+				
+				//Reset next frame:
+				currentFrameTotal = 0;
+				currentFrame++;
+				frameRoll = 1;  //Reset frame roll back to one.
 				
 			} else if (frameRoll == 2 && playerSheet[i] == STRIKE_VALUE ) {
 				//Logic should prevent this state from occurring:
@@ -52,30 +58,60 @@ public class BowlingScorer {
 				
 			} else {
 				if (frameRoll == 2) {
-					currentFrame++;
-					frameRoll = 1;
-					frameTotals[currentFrame] = currentFrameTotal;
+					currentFrameTotal += playerSheet[i];					
+					frameType.add(currentFrameTotal == STRIKE_VALUE ? SPARE : NORMAL);
+					
+					//Calculate Frame Total (a spare or no spare):
+					if (currentFrameTotal == STRIKE_VALUE) {
+						currentFrameTotal = playerSheet[i] + playerSheet[i+1];
+						frameTotals.add(currentFrameTotal);	
+						System.out.println("Roll #" + i + " : Spare added.  Points = " + currentFrameTotal);
+					} else {
+						frameTotals.add(currentFrameTotal);
+						System.out.println("Roll #" + i + " : Second Frame added (No Spare or Strike).  Points = " + currentFrameTotal);
+					}					
+					
+					//Reset next frame:
 					currentFrameTotal = 0;
+					currentFrame++;
+					frameRoll = 1;  //Reset frame roll back to one.
+					
+				} else {
+					//Role 1 of current Frame ~ no Strike or Spare:
+					currentFrameTotal += playerSheet[i];	
+					frameRoll = 2;
 				}
-				
-				currentFrameTotal += playerSheet[i];
 				
 			}
 			
 			
-			//Special for frame 10 - up to three rolls
-			
-			currentFrame++;
+			//Special logic for frame 10 - up to three rolls
+						
 		}
 		
-		// Process Player Score:
+		
+		// Process Player Score, taking into account multi-framed scoring:
 		
 		int playerTotal = 0;
 		
+		//Use the old style for/next so that I can use the iterator:
+//		int iLength = frameTotals.size(); 
+		for (int i = 0; i < frameTotals.get(i); i++) {
+//			if (frameType.get(i).equals(STRIKE) && frameTotals.size() > ) {
+//				playerTotal += (frameTotals.get(i) + frameTotals.get(i+1) + frameTotals.get(i+2)); //This doesn't work, because it is individual balls, not frames that are added...
+//			} else if (frameType.get(i).equals(SPARE)) {
+//				playerTotal += (frameTotals.get(i) + frameTotals.get(i+1));
+//			} else {
+				playerTotal += frameTotals.get(i);
+				
+//			}
+		}
 		
 		
 		
-		// spit out results
+		// spit out results (ideally with roll values and scoring side by side, like an actual bowling score sheet)
+		System.out.println("\n");
+		System.out.println("\n                  P L A Y E R   S C O R E   S H E E T ");
 
 	}
 
